8f0ee6d78590bac2789db1f11a9e325210cb93d1
KurniaDwiAgustin <kurniaagustin926@gmail.com>
2026-04-17 00:46:52 +0800

    Add files via upload

---
+++ initialcodebeforeaudit/kodeburuk.py
@@ -0,0 +1,17 @@
+# kode sebelum audit (buruk tapi jalan)
+
+def f(a, b):
+    c = a + b
+    print("Hasil:", c)
+
+x = 10
+y = 5
+
+f(x, y)
+
+# duplikasi fungsi (tidak perlu)
+def f(a, b):
+    c = a + b
+    print("Jumlah:", c)
+
+f(x, y)
